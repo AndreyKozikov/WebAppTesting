defd47b82571a09f34f35c99b230484ee5968992
Andrey Kozikov <nwisdom@mail.ru>
2024-12-13 14:11:56 +0500

    Lesson 4

---
+++ seminar4/StandGB.py
@@ -0,0 +1,48 @@
+import yaml
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import logging
+
+with open("config.yaml", 'r') as stream:
+    config = yaml.safe_load(stream)
+
+
+class StandGB:
+    def __init__(self, driver):
+        self.driver = driver
+        driver.implicitly_wait(config['wait_time'])
+
+    def find_element(self, locator, time=20):
+        try:
+            return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator),
+                                                     message=f'Element {locator} not found')
+        except Exception:
+            logging.exception(f'Element {locator} find exception.')
+            return None
+
+    def wait_element_stay_invisible(self, locator, time=20):
+        return WebDriverWait(self.driver, time).until(EC.invisibility_of_element_located(locator))
+
+    def get_element_property(self, locator, prop):
+        element = self.find_element(locator)
+        if element:
+            return element.value_of_css_property(prop)
+        else:
+            logging.error(f'Property {prop} not found in element {locator}')
+            return None
+
+    def get_element_value(self, locator):
+        element = self.find_element(locator)
+        if element:
+            return element.get_attribute('value')
+        else:
+            logging.error(f'Element {locator} not found.')
+            return None
+
+    def get_web_page(self, url):
+        try:
+            return self.driver.get(url)
+        except:
+            logging.exception(f'Exception while trying to open {url}.')
+            return None
---
+++ seminar4/testoperations.py
@@ -0,0 +1,148 @@
+import time
+
+from StandGB import StandGB
+from selenium.webdriver.common.by import By
+import yaml
+import logging
+
+
+class SearchLocators:
+    ids = dict()
+    with open("locators.yaml", "r") as f:
+        locators = yaml.safe_load(f)
+    if locators["XPATH"]:
+        for locator in locators["XPATH"].keys():
+            ids[locator] = (By.XPATH, locators["XPATH"][locator])
+    if locators["CSS"]:
+        for locator in locators["CSS"].keys():
+            ids[locator] = (By.CSS_SELECTOR, locators["CSS"][locator])
+
+    # LOCATOR_LOGIN_FIELD = (By.XPATH, '//*[@id="login"]/div[1]/label/input')
+    # LOCATOR_PASSWORD_FIELD = (By.XPATH, '//*[@id="login"]/div[2]/label/input')
+    # LOCATOR_LOGIN_BUTTON = (By.XPATH, '//*[@id="login"]/div[3]/button')
+    # LOCATOR_ERROR_FIELD = (By.XPATH, '//*[@id="app"]/main/div/div/div[2]/h2')
+    # LOCATOR_CREATE_POST_BTN = (By.CSS_SELECTOR, '#create-btn')
+    # LOCATOR_TITLE_POST_FIELD = (By.XPATH, '//*[@id="create-item"]/div/div/div[1]/div/label/input')
+    # LOCATOR_DESC_POST_FIELD = (By.XPATH, '//*[@id="create-item"]/div/div/div[2]/div/label/span/textarea')
+    # LOCATOR_CONTENT_POST_FIELD = (By.XPATH, '//*[@id="create-item"]/div/div/div[3]/div/label/span/textarea')
+    # LOCATOR_SAVE_POST_BTN = (By.XPATH, '//*[@id="create-item"]/div/div/div[7]/div/button')
+    # LOCATOR_TITLE_AFTER_CREATE = (By.XPATH, '//*[@id="app"]/main/div/div[1]/h1')
+    # LOCATOR_SUCCESS_LOGIN = (By.XPATH, '//*[@id="app"]/main/nav/ul/li[3]/a')
+    # LOCATOR_HREF_CONTACT_US = (By.XPATH, '//*[@id="app"]/main/nav/ul/li[2]/a')
+    # LOCATOR_H1_CONTACT_US = (By.XPATH, '//*[@id="app"]/main/div/div/h1')
+    # LOCATOR_CONTACT_YOU_NAME_FIELD = (By.XPATH, '//*[@id="contact"]/div[1]/label/input')
+    # LOCATOR_CONTACT_YOU_EMAIL_FIELD = (By.XPATH, '//*[@id="contact"]/div[2]/label/input')
+    # LOCATOR_CONTACT_CONTENT_FIELD = (By.XPATH, '//*[@id="contact"]/div[3]/label/span/textarea')
+    # LOCATOR_CONTACT_US_BTN = (By.XPATH, '//*[@id="contact"]/div[4]/button')
+
+class OperationsHelper(StandGB):
+    def check_success_login(self):
+        return self.find_element(SearchLocators.ids["LOCATOR_SUCCESS_LOGIN"]).text
+
+    def enter_login(self, login):
+        logging.info(f"Send '{login}' to element {SearchLocators.ids['LOCATOR_LOGIN_FIELD']}")
+        login_field = self.find_element(SearchLocators.ids["LOCATOR_LOGIN_FIELD"])
+        if not login_field:
+            logging.error("Login field not found")
+            return False
+        try:
+            login_field.clear()
+            login_field.send_keys(login)
+        except:
+            logging.exception("Exception while operation with login field")
+
+
+    def enter_password(self, password):
+        password_field = self.find_element(SearchLocators.ids["LOCATOR_PASSWORD_FIELD"])
+        if not password_field:
+            logging.error("Password field not found")
+            return False
+        try:
+            password_field.clear()
+            password_field.send_keys(password)
+        except:
+            logging.exception("Exception while operation with password field")
+
+    def click_login_button(self, wait=False):
+        try:
+            self.find_element(SearchLocators.ids["LOCATOR_LOGIN_BUTTON"]).click()
+            if wait:
+                self.wait_element_stay_invisible(SearchLocators.ids["LOCATOR_LOGIN_BUTTON"])
+        except:
+            logging.exception("Exception while click login button")
+
+    def get_error_message(self):
+        try:
+            error_text = self.find_element(SearchLocators.ids["LOCATOR_ERROR_FIELD"]).text
+            return error_text
+        except:
+            return None
+
+    def click_create_post_button(self):
+        try:
+            self.find_element(SearchLocators.ids["LOCATOR_CREATE_POST_BTN"]).click()
+            self.wait_element_stay_invisible(SearchLocators.ids["LOCATOR_CREATE_POST_BTN"])
+        except:
+            logging.exception("Exception while click create post button")
+
+    def enter_title_post(self, title):
+        title_field = self.find_element(SearchLocators.ids["LOCATOR_TITLE_POST_FIELD"])
+        if not title_field:
+            logging.error("Title field in post creation form not found")
+            return False
+        try:
+            title_field.clear()
+            title_field.send_keys(title)
+        except:
+            logging.exception("Exception while operation with title field in post creation form")
+
+    def enter_content_post(self, content):
+        content_field = self.find_element(SearchLocators.ids["LOCATOR_CONTENT_POST_FIELD"])
+        content_field.clear()
+        content_field.send_keys(content)
+
+    def enter_desc_post(self, desc):
+        desc_field = self.find_element(SearchLocators.ids["LOCATOR_DESC_POST_FIELD"])
+        desc_field.clear()
+        desc_field.send_keys(desc)
+
+    def click_save_post_button(self):
+        self.find_element(SearchLocators.ids["LOCATOR_SAVE_POST_BTN"]).click()
+        self.wait_element_stay_invisible(SearchLocators.ids["LOCATOR_SAVE_POST_BTN"])
+
+    def check_title_after_create(self):
+        self.wait_element_stay_invisible(SearchLocators.ids["LOCATOR_SAVE_POST_BTN"])
+        title = self.find_element(SearchLocators.ids["LOCATOR_TITLE_AFTER_CREATE"]).text
+        return  title
+
+    def click_link_contact(self):
+        self.find_element(SearchLocators.ids["LOCATOR_HREF_CONTACT_US"]).click()
+
+    def check_contact_form_load(self):
+        return self.find_element(SearchLocators.ids["LOCATOR_H1_CONTACT_US"]).text
+
+    def enter_contact_name(self, name):
+        name_field = self.find_element(SearchLocators.ids["LOCATOR_CONTACT_YOU_NAME_FIELD"])
+        name_field.clear()
+        name_field.send_keys(name)
+
+    def enter_contact_email(self, email):
+        email_field = self.find_element(SearchLocators.ids["LOCATOR_CONTACT_YOU_EMAIL_FIELD"])
+        email_field.clear()
+        email_field.send_keys(email)
+
+    def enter_contact_content(self, contact):
+        content_filed = self.find_element(SearchLocators.ids["LOCATOR_CONTACT_CONTENT_FIELD"])
+        content_filed.clear()
+        content_filed.send_keys(contact)
+
+    def click_contact_us_btn(self):
+        self.find_element(SearchLocators.ids["LOCATOR_CONTACT_US_BTN"]).click()
+
+    def check_filled_contact_form(self):
+        name = self.get_element_value(SearchLocators.ids["LOCATOR_CONTACT_YOU_NAME_FIELD"])
+        email = self.get_element_value(SearchLocators.ids["LOCATOR_CONTACT_YOU_EMAIL_FIELD"])
+        content = self.get_element_value(SearchLocators.ids["LOCATOR_CONTACT_CONTENT_FIELD"])
+        return name, email, content
+
+
